272f0c1aa5e67aa32cccb4627dc0e5ef55010028
Mohan Sharma <mohansharma_48@live.com>
2023-01-20 23:45:40 +0545

    update

--- index.js
+++ index.js
@@ -124,6 +124,176 @@ $(document).ready(function () {
         $('.decrptn7').slideUp();
     });
 
+    $('#d9').on("click", function () {
+        $('#box9').slideUp();
+        $('.decrptn8').slideDown();
+    });
+
+    $('#b9').on("click", function () {
+        $('#box9').slideDown();
+        $('.decrptn8').slideUp();
+    });
+
+    $('#d10').on("click", function () {
+        $('#box10').slideUp();
+        $('.decrptn9').slideDown();
+    });
+
+    $('#b10').on("click", function () {
+        $('#box10').slideDown();
+        $('.decrptn9').slideUp();
+    });
+
+    $('#d11').on("click", function () {
+        $('#box11').slideUp();
+        $('.decrptn10').slideDown();
+    });
+
+    $('#b11').on("click", function () {
+        $('#box11').slideDown();
+        $('.decrptn10').slideUp();
+    });
+
+    $('#d12').on("click", function () {
+        $('#box12').slideUp();
+        $('.decrptn11').slideDown();
+    });
+
+    $('#b12').on("click", function () {
+        $('#box12').slideDown();
+        $('.decrptn11').slideUp();
+    });
+
+    $('#d13').on("click", function () {
+        $('#box13').slideUp();
+        $('.decrptn12').slideDown();
+    });
+
+    $('#b13').on("click", function () {
+        $('#box13').slideDown();
+        $('.decrptn12').slideUp();
+    });
+
+    $('#d14').on("click", function () {
+        $('#box14').slideUp();
+        $('.decrptn13').slideDown();
+    });
+
+    $('#b14').on("click", function () {
+        $('#box14').slideDown();
+        $('.decrptn13').slideUp();
+    });
+
+    $('#d15').on("click", function () {
+        $('#box15').slideUp();
+        $('.decrptn14').slideDown();
+    });
+
+    $('#b15').on("click", function () {
+        $('#box15').slideDown();
+        $('.decrptn14').slideUp();
+    });
+
+    $('#d16').on("click", function () {
+        $('#box16').slideUp();
+        $('.decrptn15').slideDown();
+    });
+
+    $('#b16').on("click", function () {
+        $('#box16').slideDown();
+        $('.decrptn15').slideUp();
+    });
+
+    $('#d17').on("click", function () {
+        $('#box17').slideUp();
+        $('.decrptn16').slideDown();
+    });
+
+    $('#b17').on("click", function () {
+        $('#box17').slideDown();
+        $('.decrptn16').slideUp();
+    });
+
+    $('#d18').on("click", function () {
+        $('#box18').slideUp();
+        $('.decrptn17').slideDown();
+    });
+
+    $('#b18').on("click", function () {
+        $('#box18').slideDown();
+        $('.decrptn17').slideUp();
+    });
+
+    $('#d19').on("click", function () {
+        $('#box19').slideUp();
+        $('.decrptn18').slideDown();
+    });
+
+    $('#b19').on("click", function () {
+        $('#box19').slideDown();
+        $('.decrptn18').slideUp();
+    });
+
+    $('#d20').on("click", function () {
+        $('#box20').slideUp();
+        $('.decrptn19').slideDown();
+    });
+
+    $('#b20').on("click", function () {
+        $('#box20').slideDown();
+        $('.decrptn19').slideUp();
+    });
+
+    $('#d21').on("click", function () {
+        $('#box21').slideUp();
+        $('.decrptn20').slideDown();
+    });
+
+    $('#b21').on("click", function () {
+        $('#box21').slideDown();
+        $('.decrptn20').slideUp();
+    });
+
+    $('#d22').on("click", function () {
+        $('#box22').slideUp();
+        $('.decrptn21').slideDown();
+    });
+
+    $('#b22').on("click", function () {
+        $('#box22').slideDown();
+        $('.decrptn21').slideUp();
+    });
+
+    $('#d23').on("click", function () {
+        $('#box23').slideUp();
+        $('.decrptn22').slideDown();
+    });
+
+    $('#b23').on("click", function () {
+        $('#box23').slideDown();
+        $('.decrptn22').slideUp();
+    });
+
+    $('#b24').on("click", function () {
+        $('#box24').slideDown();
+        $('.decrptn23').slideUp();
+    });
+
+    $('#d24').on("click", function () {
+        $('#box24').slideUp();
+        $('.decrptn23').slideDown();
+    });
+
+    $('#b25').on("click", function () {
+        $('#box25').slideDown();
+        $('.decrptn24').slideUp();
+    });
+
+    $('#d25').on("click", function () {
+        $('#box25').slideUp();
+        $('.decrptn24').slideDown();
+    });
+
 });
 
 
