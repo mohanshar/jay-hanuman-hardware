2e5992fe249adbdfb5cf004dc3735dc299d1b769
https://github.com/mohanshar <mohangraunt@outlook.com>
2024-02-03 16:47:11 +0545

    first init

--- Photos/HTML_CSS - Test Calculator Online Editor and JavaScript Compiler _ Sololearn_files/authorize-v5.js
+++ Photos/HTML_CSS - Test Calculator Online Editor and JavaScript Compiler _ Sololearn_files/authorize-v5.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿//(function () {
 //    function hasOwin() {
 //        return document.cookie.indexOf('LoginState=1') > -1;
@@ -23,3 +24,30 @@
 //    }
 //})();
 
+=======
+﻿//(function () {
+//    function hasOwin() {
+//        return document.cookie.indexOf('LoginState=1') > -1;
+//    }
+
+//    function hasTokens() {
+//        var user = JSON.parse(localStorage.getItem('user'));
+//        var accessToken = JSON.parse(localStorage.getItem('accessToken'));
+//        return !!(user && user.data && accessToken && accessToken.data);
+//    }
+
+//    if (
+//        (hasOwin() && !hasTokens()) || (hasTokens() && !hasOwin())
+//    ) {
+//        localStorage.removeItem('user');
+//        localStorage.removeItem('accessToken');
+//        localStorage.removeItem('refreshToken');
+//        localStorage.removeItem('isDark');
+//        localStorage.removeItem('expiresIn');
+//        localStorage.removeItem('msAccessToken');
+//        localStorage.removeItem('serviceAccessToken');
+//        window.location.href = "/User/Logout?returnUrl=" + window.location.pathname;
+//    }
+//})();
+
+>>>>>>> 4ff3e4a (Initial commit)
--- Photos/HTML_CSS - Test Calculator Online Editor and JavaScript Compiler _ Sololearn_files/logconsent.ashx
+++ Photos/HTML_CSS - Test Calculator Online Editor and JavaScript Compiler _ Sololearn_files/logconsent.ashx
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 CookieConsent.setCookie("{stamp:%27q2P+cnS71gNDW7BNstbnK1dAFKv3LzFrcDbCwHjMUkEIN+zkECLlxA==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1673367175965%2Cregion:%27np%27}", new Date(1704903175965), '/', '', '');
 if (typeof CookieConsent.removeBulkReset!='undefined') {CookieConsent.removeBulkReset();}
 CookieConsent.userCountry = 'np';
 CookieConsent.init();
 if (typeof CookieConsent.resetCookies!='undefined') {CookieConsent.resetCookies();}
+=======
+CookieConsent.setCookie("{stamp:%27q2P+cnS71gNDW7BNstbnK1dAFKv3LzFrcDbCwHjMUkEIN+zkECLlxA==%27%2Cnecessary:true%2Cpreferences:true%2Cstatistics:true%2Cmarketing:true%2Cmethod:%27explicit%27%2Cver:1%2Cutc:1673367175965%2Cregion:%27np%27}", new Date(1704903175965), '/', '', '');
+if (typeof CookieConsent.removeBulkReset!='undefined') {CookieConsent.removeBulkReset();}
+CookieConsent.userCountry = 'np';
+CookieConsent.init();
+if (typeof CookieConsent.resetCookies!='undefined') {CookieConsent.resetCookies();}
+>>>>>>> 4ff3e4a (Initial commit)
